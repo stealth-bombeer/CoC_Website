5d8874bec779131a0bbfe1eff58507dbc70b58d6
KUNAL <goudanikunal3@gmail.com>
2023-11-25 22:36:51 +0530

    next app intialised

---
+++ client/pages/index.js
@@ -0,0 +1,9 @@
+import styles from "../styles/Home.module.css"
+
+export default function Home() {
+    return (
+        <div className="container mx-auto">
+            <h1 className="py-4 px-4 font-bold text-2xl">Home</h1>
+        </div>
+    )
+}
